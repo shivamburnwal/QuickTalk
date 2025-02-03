05b2374674f232d7b971e916bc9618b64f502368
shivamburnwal <shivam29a@gmail.com>
2025-02-03 22:57:34 +0530

    Add Admin Feature in Group Chatrooms.

--- QuickTalk.Api/Models/UserChatroom.cs
+++ QuickTalk.Api/Models/UserChatroom.cs
@@ -1,10 +1,17 @@
 ﻿namespace QuickTalk.Api.Models
 {
+    public enum ChatroomRole
+    {
+        Member,
+        Admin
+    }
+
     public class UserChatroom
     {
         public int UserID { get; set; }
         public int ChatroomID { get; set; }
         public DateTime JoinDate { get; set; } = DateTime.UtcNow;
+        public ChatroomRole Role { get; set; }  // New field to store user role
 
         public User User { get; set; }
         public Chatroom Chatroom { get; set; }
